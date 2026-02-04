4377b60d2bc593b707176cd56a067b1481c941f2
gyaiiiin0727 <shuheisug@gmail.com>
2026-02-04 18:03:50 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE_NAME = 'pwa-cache-v12.2';
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
