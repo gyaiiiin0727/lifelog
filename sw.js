21084855d2d98d33f525ee5b4a0ad7911165e929
gyaiiiin0727 <shuheisug@gmail.com>
2026-02-06 11:33:54 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,31 @@
-const CACHE_NAME='pwa-cache-v12.3';
-self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k)))));
-  self.clients.claim();
+/* Lifelog SW v3 */
+const CACHE_NAME = 'lifelog-cache-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(()=>self.skipWaiting())
+  );
 });
-self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k)=> (k===CACHE_NAME)?null:caches.delete(k)))).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((resp)=>{
+      const copy = resp.clone();
+      caches.open(CACHE_NAME).then((cache)=>cache.put(event.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(()=>cached))
+  );
 });
