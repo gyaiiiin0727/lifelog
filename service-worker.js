46de2e781b28124838a11748aa0d3818f5ba3473
gyaiiiin0727 <shuheisug@gmail.com>
2026-02-08 18:46:58 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,16 @@
 // ===== lifelog service worker (cache-bump) =====
-const CACHE_NAME = 'lifelog-cache-v20260208092758';
-const CORE_ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'lifelog-cache-v20260208094242';
+const CORE_ASSETS = ['./','./index.html','./manifest.json'];
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).catch(() => {})
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)).catch(()=>{}));
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE_NAME) ? caches.delete(k) : Promise.resolve()))
-    ).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE_NAME) ? caches.delete(k) : Promise.resolve())))
+      .then(() => self.clients.claim())
   );
 });
 
@@ -27,13 +20,11 @@ self.addEventListener('fetch', (event) => {
     event.respondWith(
       fetch(req).then(res => {
         const copy = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy)).catch(() => {});
+        caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy)).catch(()=>{});
         return res;
       }).catch(() => caches.match('./index.html'))
     );
     return;
   }
-  event.respondWith(
-    caches.match(req).then(hit => hit || fetch(req))
-  );
+  event.respondWith(caches.match(req).then(hit => hit || fetch(req)));
 });
